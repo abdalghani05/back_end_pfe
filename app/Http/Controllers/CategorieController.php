c723ca777ca08c1571bb0a162a65543ab09c92e1
abdalghani05 <anairi2005@gmail.com>
2023-05-16 23:23:52 +0100

    ajouter controller categorie et tester

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Categorie;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Categorie::all();
+        return response()->json($categories);
+
+    }
+    public function store(Request $request)
+    {
+        return response()->json(Categorie::create($request->all()));
+
+
+        /*  $author = new Author([
+              'nom' => $request->input('nom'),
+              'prenom' => $request->input('prenom'),
+              'description' => $request->input('description'),
+          ]);
+          $author->save();
+          return response()->json('Author created!');*/
+    }
+
+    public function show($id)
+    {
+        $contact = Categorie::find($id);
+        return response()->json($contact);
+    }
+
+    public function update(Request $request,$id)
+    {
+        /*   $request ->validate([
+               'nom' =>'require' ,
+               'prenom'=>'require' ,
+               'description'=>'require'
+           ]);
+
+           $author->fill($request->post())->update();
+           return response()->json(['message'=>'Author updated!']);*/
+
+        $categorie = Categorie::find($id);
+        $categorie->update($request->all());
+        return response()->json('Categorie updated');
+    }
+
+    public function destroy($id)
+    {
+        $categorie = Categorie::findOrFail($id);
+        $categorie->delete();
+        return response()->json('Categorie deleted');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\CategorieController;
 
-
+// Route Authors
 Route::get('/Authors',[AuthorsController::class,'index']);
 Route::post('/Authors/save',[AuthorsController::class, 'store']);
 Route::put('/Authors/update/{id}',[AuthorsController::class, 'update']);
 Route::delete('/Authors/delete/{id}',[AuthorsController::class, 'destroy']);
+
+
+//Route Categorie
+Route::get('/categorie',[CategorieController::class,'index']);
+Route::post('/categorie/save',[CategorieController::class, 'store']);
+Route::put('/categorie/update/{id}',[CategorieController::class, 'update']);
+Route::delete('/categorie/delete/{id}',[CategorieController::class, 'destroy']);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
